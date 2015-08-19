4db3c7a85f6d1a5a3321d10247ad2e6e09261148
Jef Claes <jef.claes@gmail.com>
2015-08-19 21:52:24 +0200

    Initial commit

---
+++ src/Casino.Game/Casino.Game/Domain.fs
@@ -0,0 +1,84 @@
+﻿namespace Casino.Game
+
+module Domain =
+
+    type Box = 
+        | Yellow 
+        | Green 
+        | Red 
+        | Blue 
+        | White 
+        | Purple      
+
+     type RandomNumber = 
+        | Num1 
+        | Num2 
+        | Num3 
+        | Num4 
+        | Num5 
+        | Num6 
+
+    type BoxValue = 
+        | Win of WinFactor 
+        | Lose  
+    and WinFactor = decimal
+
+    type SpinResult = { 
+        Box : Box 
+        Value : BoxValue
+    }         
+
+    type RNG = unit -> RandomNumber
+    type MapToBox = RandomNumber -> Box
+    type MapToValue = Box -> BoxValue  
+    type Spin = RNG -> MapToBox -> MapToValue -> SpinResult
+
+    let boxes = [ Yellow; Green; Red; Blue; White; Purple ]   
+
+    let rng () =         
+        let rnd = new System.Random()
+        let n = rnd.Next(1, 7)        
+
+        match n with
+        | 1 -> Num1
+        | 2 -> Num2
+        | 3 -> Num3
+        | 4 -> Num4
+        | 5 -> Num5
+        | 6 -> Num6
+        | _ -> failwith "Random number could not be mapped"
+
+    let mapToBox rnd =   
+        match rnd with
+        | Num1 -> Yellow 
+        | Num2 -> Green
+        | Num3 -> Red
+        | Num4 -> Blue  
+        | Num5 -> White
+        | Num6 -> Purple  
+
+    let mapToValue box =
+        match box with
+        | Red -> Win 6M
+        | _ -> Lose
+
+    let mapToValueWithHouseEdge box =
+        match box with
+        | Red -> Win 5M
+        | _ -> Lose
+
+    let mapToBoxWithNearMisses rnd =
+        match rnd with
+        | Num1 | Num2 -> Green 
+        | Num3 -> Red
+        | Num4 | Num5 | Num6 -> Blue    
+
+    let spin : Spin = fun rng mapToBox mapToValue ->                              
+        let box = rng() |> mapToBox
+        let value = box |> mapToValue                       
+        
+        { Box = box; Value = value }
+    
+    let spinWithoutNearMisses () = spin rng mapToBox mapToValue 
+    let spinWithNearMisses () = spin rng mapToBoxWithNearMisses mapToValueWithHouseEdge 
+
---
+++ src/Casino.Game/Casino.Game/Program.fs
@@ -0,0 +1,97 @@
+﻿namespace Casino.Game
+
+open System
+open Casino.Game.Domain
+
+module Program =
+
+    let systemColor box =
+        match box with
+        | Yellow -> ConsoleColor.Yellow
+        | Green -> ConsoleColor.Green
+        | Red -> ConsoleColor.Red
+        | Blue -> ConsoleColor.Blue
+        | White -> ConsoleColor.White
+        | Purple -> ConsoleColor.Magenta
+
+    let drawBox box hit =             
+        Console.ForegroundColor <- systemColor <| box             
+            
+        printf "[%s] " <| match hit with | true -> "X" | false -> " "
+
+        Console.ResetColor()                    
+
+    let youLost () = printfn "Unlucky, you lost."
+    let youWon factor = printfn "You won your stake x %A" factor
+
+    let render spinResult =
+        printfn ""
+
+        boxes 
+        |> Seq.iter (fun b -> 
+            let hit = (b = spinResult.Box)
+            drawBox b hit)              
+            
+        [1 .. 2] 
+        |> Seq.iter (fun _ -> printfn " ")
+
+        match spinResult.Value with
+        | Win factor -> 
+            [1 .. 3] 
+            |> Seq.iter(fun _ -> youWon factor)
+        | Lose -> youLost()
+
+    let rec start (shouldContinue : unit -> bool) =
+        spinWithNearMisses() |> render
+
+        if shouldContinue() then 
+            start shouldContinue
+
+    let drawEmptyBox box =         
+        Console.ForegroundColor <- systemColor box
+            
+        printf "[ ] "
+
+        Console.ResetColor()
+
+    let pct x y = float x / float y * float 100        
+
+    let distribution desc times (spin:unit->SpinResult) =    
+        printfn ""
+        printfn desc
+        printfn ""
+
+        [ 1 .. times ] 
+        |> Seq.map (fun _ -> spin()) 
+        |> Seq.countBy (fun result -> result.Box)
+        |> Seq.map (fun (k, v) -> (k, pct v times))  
+        |> fun distribution -> 
+            for element in distribution do
+                let box, percentage = element
+                Console.ForegroundColor <- systemColor box               
+                printfn "%A = %A%%" box percentage
+                Console.ResetColor()     
+
+    let houseEdge oddsOfWinning winnings oddsOfLosing stake =            
+        let ( x : decimal ) = oddsOfWinning * winnings - oddsOfLosing * stake
+        Math.Round(x, 2)
+
+    [<EntryPoint>]
+    let main argv = 
+        printfn ""  
+        
+        spinWithoutNearMisses |> distribution "Spin without near misses" 1000000
+        spinWithNearMisses |> distribution "Spin with near misses" 1000000
+
+        start (fun () ->
+            Console.WriteLine()
+            Console.WriteLine("Go again?")            
+            Console.ReadLine() = "yes!")                                 
+
+        printfn ""
+
+        houseEdge ( 1M / 6M ) 4M ( 5M / 6M ) 1M  |> printfn "House edge = %A"        
+
+        Console.ReadLine() |> ignore
+
+        0
